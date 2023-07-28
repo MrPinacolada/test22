171c2c6ee4184f872c7d4bd4e4a128cadae9d574
MrPinacolada <vasiasko112@gmail.com>
2023-07-28 13:32:29 +0400

    none

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,4 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === "production" ? "/test22/" : "/",
   transpileDependencies: true,
-  configureWebpack: {
-    output: {
-      library: "weathertest22",
-      libraryTarget: "umd",
-    },
-  },
 });
